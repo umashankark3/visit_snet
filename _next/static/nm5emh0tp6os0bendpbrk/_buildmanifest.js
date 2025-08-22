7fa3ad7d13701385f3d28e97c98eb0940591febe
Your Name <your.email@example.com>
2025-08-22 07:03:49 +0530

    Add all project files

---
+++ _next/static/nm5emh0tp6os0bendpbrk/_buildmanifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/ee8b1517-b39f3492fd2cc756.js","static/chunks/672-a00cf66e77ee74ad.js","static/chunks/pages/index-b340547e69c4a06e.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
